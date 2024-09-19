eb6afa6aa10bdd232bba2a145132b42e98651120
hyobin77 <gyqls0509@kumoh.ac.kr>
2024-09-19 14:27:56 +0900

    :poop: CSS

---
+++ src/main/kotlin/kr/ac/kumoh/s20230358/w24w0302Css/W24w0302CssApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20230358.w24w0302Css
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w0302CssApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w0302CssApplication>(*args)
+}
